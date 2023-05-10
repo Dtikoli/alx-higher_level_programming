edc854b8d524d295f454b79aa60fc391a9da2f51
Dtikoli <tikolidavid@yahoo.com>
2023-05-10 05:08:47 -0700

    Revision to combo5 and combo6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 for n in range(0, 90):
-    if n / 10 < n % 10:
-        if n != 89:
-            print("{:02d}".format(n), end=", ")
+    if n / 10 < n % 10 and n != 89:
+        print("{:02d}".format(n), end=", ")
 print("89")
