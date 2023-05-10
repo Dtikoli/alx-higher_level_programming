4339dd180109235bae50dc7529c21e92b404eabb
Dtikoli <tikolidavid@yahoo.com>
2023-05-10 04:56:08 -0700

    Update to combo5 and combo6

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print(", ".join("{:02d}".format(n) for n in range(0, 100)))
+combo5 = ", ".join(f"{n:02d}" for n in range(0, 100))
+print(combo5)
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,5 @@
 for n in range(0, 90):
     if n / 10 < n % 10:
         if n != 89:
-            print("{:02d}, ".format(n), end='')
-        else:
-            print("{:02d}".format(n))
+            print(f"{n:02d}", end=", ")
+print("89")
