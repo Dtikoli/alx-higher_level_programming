7670756fbaca9e7efe85d66425c1205e3bfdaaa2
Dtikoli <tikolidavid@yahoo.com>
2023-05-13 03:15:18 -0700

    Printing python list in C code

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,23 +1,20 @@
 #include <Python.h>
-#include <stdio.h>
+#include <object.h>
+#include <listobject.h>
+
 /**
- * print_python_list_info - prints python list info
- * @p: PyObject
- * Return: Nothing
- */
+* print_python_list_info - prints some basic info about pytohn list
+* @p: python object
+**/
 
 void print_python_list_info(PyObject *p)
 {
-	Py_ssize_t size, alloc, i;
+	long int size = PyList_Size(p);
+	int i;
+	PyListObject *obj = (PyListObject *)p;
 
-	size = PyList_Size(p);
-	alloc = ((PyListObject *)p)->allocated;
-	printf("[*] Size of the Python List = %ld\n", size);
-	printf("[*] Allocated = %ld\n", alloc);
+	printf("[*] Size of the Python List = %li\n", size);
+	printf("[*] Allocated = %li\n", obj->allocated);
 	for (i = 0; i < size; i++)
-	{
-		printf("Element %ld: %s\n",
-		       i,
-		       (PY_TYPE(PyList_GetItem(p, i)))->tp_name);
-	}
+		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
 }
