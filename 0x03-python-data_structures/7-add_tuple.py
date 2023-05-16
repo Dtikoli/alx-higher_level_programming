53b0c35a9994402484e3f0e259631462751d7000
Dtikoli <tikolidavid@yahoo.com>
2023-05-15 19:54:48 -0700

    Summing numbers in tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,4 +2,5 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     a = tuple_a + (0, 0)
     b = tuple_b + (0, 0)
-    return (a[0] + b[0], a[1] + b[1])
+    sums = tuple(i + j for i, j in zip(a, b))
+    return sums[0:2]
--- 0x03-python-data_structures/7-add_tuple_p1.py
+++ 0x03-python-data_structures/7-add_tuple_p1.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    a = list(tuple_a) + [0]*2
-    b = list(tuple_b) + [0]*2
-    sums = [i + j for i, j in zip(a, b)]
-    return tuple(sums[0:2])
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
+    return (a[0] + b[0], a[1] + b[1])
