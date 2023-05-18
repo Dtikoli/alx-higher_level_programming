2ba5c7471d4ddcf1c939b3215e814ecb6a164c07
Dtikoli <tikolidavid@yahoo.com>
2023-05-18 08:22:49 -0700

    Returning the weighted average of all integers tuple with alt

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,9 +2,6 @@
 def weight_average(my_list=[]):
     if not my_list:
         return 0
-    numerator = 0
-    denominator = 0
-    for tup in my_list:
-        numerator += tup[0] * tup[1]
-        denominator += tup[1]
-    return (numerator / denominator)
+    num = sum([tup[0] * tup[1] for tup in my_list])
+    denom = sum([tup[1] for tup in my_list])
+    return (num / denom)
