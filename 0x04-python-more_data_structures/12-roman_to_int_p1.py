bbd23e0c9ee67f9ba7d6adb27b3bc21c94235134
Dtikoli <tikolidavid@yahoo.com>
2023-05-19 04:40:58 -0700

    Converting a Roman numeral in a string form to integer

--- 0x04-python-more_data_structures/12-roman_to_int_p1.py
+++ 0x04-python-more_data_structures/12-roman_to_int_p1.py
@@ -4,7 +4,7 @@ def roman_to_int(roman_string):
         return 0
     number = 0
     decimal = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
-    value_list = [decimal.get(numeral) for numeral in reversed(roman_string)]
-    for value in value_list:
-        number += value if number < 5 * value else -value
+    for numeral in reversed(roman_string):
+        value = decimal.get(numeral)
+        number += value if number < value * 5 else -value
     return number
