98acf502070c6d0f5abba37916c3a6b9d63790bb
David Tikoli <122807828+Dtikoli@users.noreply.github.com>
2023-06-02 13:18:01 +0000

    Update 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    a = tuple_a + (0, 0)
-    b = tuple_b + (0, 0)
+    a = tuple_a + (0,) * 2
+    b = tuple_b + (0,) * 2
     sums = tuple(i + j for i, j in zip(a, b))
     return sums[0:2]
