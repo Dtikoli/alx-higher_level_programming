24d05e2a66c0bc20721a3ac2bf1459fc8ff066d9
Dtikoli <tikolidavid@yahoo.com>
2023-06-03 20:46:01 -0700

    Print square - update

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -9,13 +9,12 @@ def print_square(size):
     the length and width of the square.
     """
 
-    if type(size) is not int:
+    if not isinstance(size, int):
         raise TypeError("size must be an integer")
 
     if size < 0:
         raise ValueError("size must be >= 0")
 
     for i in range(size):
-        for j in range(size):
-            print('#', end='')
+        [print("#", end="") for j in range(size)]
         print()
