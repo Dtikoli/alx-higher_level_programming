33cab8ead5cc39fbd62baae6473e888cda55463e
Dtikoli <tikolidavid@yahoo.com>
2023-06-14 13:50:15 -0700

    To JSON string - update

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""A file IO module that defines a string-to-JSON function."""
+"""A module that defines a JSON serializing function."""
 import json
 
 
 def to_json_string(my_obj):
-    """Returns the JSON representation of a string object."""
+    """Returns the JSON string representation of an object."""
     return json.dumps(my_obj)
