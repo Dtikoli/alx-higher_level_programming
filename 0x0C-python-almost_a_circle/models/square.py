832520866b049b9f17fe42f40fba708d9cdf51ef
Dtikoli <tikolidavid@yahoo.com>
2023-06-18 06:22:58 -0700

    Update to square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,22 +9,26 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         """Initialization an instance of Square"""
-        super().__init__(size, size, x, y, id)
+        self.setter_validation("size, value)
+	super().__init__(size, size, x, y, id)
+	self.size = size
 
     @property
     def size(self):
         """Gets the size of the Square"""
-        return self.width
+        return self.__width
 
     @size.setter
     def size(self, value):
         """Sets the size of the Square"""
-        self.width = value
-        self.height = value
+        self.setter_validation("size, value)
+        self.__size = value
+        self.__width = value
+        self.__height = value
 
     def __str__(self):
         """String representation of square"""
-        return f"[Square] ({self.id})self.x}/{self.y} - {self.width}"
+        return f"[Square] ({self.id})self.x}/{self.y} - {self.size}"
 
     def update(self, *args, **kwargs):
         """Updates the arguments in the class"""
@@ -44,7 +48,7 @@ class Square(Rectangle):
                     setattr(self, key, value)
 
     def to_dictionary(self):
-        """returns dictonary"""
+        """returns dictonary representation of square"""
         return {
             "id": self.id,
             "size": self.size,
