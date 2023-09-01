671bf9d8d8b619eb7741172ce1f64093c7cb7e54
Dtikoli <tikolidavid@yahoo.com>
2023-09-01 04:20:39 -0700

    What is my status? #1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Fetches https://alx-intranet.hbtn.io/status """
+import requests
+
+
+if __name__ == "__main__":
+    request = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print(f"\t- type: {type(request.text))}")
+    print(f"\t- content: {request.text}")
