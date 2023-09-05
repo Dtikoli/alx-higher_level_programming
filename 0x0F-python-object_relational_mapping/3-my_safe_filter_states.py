e2e5040dedf92fc3a75348808a88367be5538c7a
Dtikoli <tikolidavid@yahoo.com>
2023-09-05 06:42:41 -0700

    Safe filter - SQL Injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -11,8 +11,7 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = conn.cursor()
-    match = sys.argv[4]
-    cur.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
+    cur.execute("SELECT * FROM states WHERE name LIKE %s", (sys.argv[4], ))
     rows = cur.fetchall()
     for row in rows:
         print(row)
