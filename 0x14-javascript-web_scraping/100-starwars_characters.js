36639d6165bb2e1deefc25c6fc34d81ec7911212
Dtikoli <tikolidavid@yahoo.com>
2023-09-26 11:07:07 -0700

    Who was playing in this movie?

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,15 +1,16 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-request(url, (err, response, body) => {
+request(url, (err, response, content) => {
   if (!err) {
-    const characters = JSON.parse(body).characters;
+    const characters = JSON.parse(content).characters;
     characters.forEach(character => {
-      request(character, (error, resp, body) => {
+      request(character, (error, resp, content) => {
         if (!error) {
-          const name = JSON.parse(body).name;
+          const name = JSON.parse(content).name;
           console.log(name);
         }
       });
