70c7392846d4145c022a36efa2d8a702cfebb042
Dtikoli <tikolidavid@yahoo.com>
2023-10-04 10:16:59 -0700

    Star Wars movies - update

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -6,3 +6,15 @@ $(document).ready(function() {
   });
 });
 
+
+/*
+Alternative: Using .get is correct since data is specified in JSON format in the URL.
+
+$(document).ready(function() {
+  $.getJSON("https://swapi-api.alx-tools.com/api/films/?format=json", function(data) {
+    $.each(data.results, function(index, movie) {
+      $('<li>').text(movie.title).appendTo('#list_movies');
+    });
+  });
+});
+*/
